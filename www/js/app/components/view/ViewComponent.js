cbe44571de58b46963a7cad9839d033e727fe934
jaimin pandya <jaiminmoslake7020@gmail.com>
2020-03-31 19:07:46 -0700

    New changes

--- www/js/app/components/view/ViewComponent.js
+++ www/js/app/components/view/ViewComponent.js
@@ -8,8 +8,6 @@ class ViewComponent extends BaseComponent{
     init(){
         consoleAlert( 'Viewe Component configureVue');
 
-        //this.configureVue();
-
         consoleAlert( 'Viewe Component Loaded');
 
         if( false ){
@@ -28,23 +26,16 @@ class ViewComponent extends BaseComponent{
 
     }
 
-    configureVue(){
-        Vue.config.productionTip = false;
+    replaceAppScreen( template ){
+        consoleAlert( template )
+        document.getElementById('app').innerHTML = template ;
+        consoleAlert( document.getElementById('app').innerHTML  );
     }
 
     showSplashScreen(){
 
-        //
-        // var app = new Vue({
-        //     el: '#screen-container',
-        //     template: '<div id="screen-container"><div class="screen"  id="splash-screen"><div class=" spalsh-screen-image  spalsh-screen "></div></div></div>'
-        // });
-        //
-
         let template ='<div id="screen-container"><div class="screen"  id="splash-screen"><div class=" spalsh-screen-image  spalsh-screen "></div></div></div>';
-        document.getElementById('app').innerHTML = template ;
-
-        consoleAlert(  document.getElementById('app').innerHTML );
+        this.replaceAppScreen( template );
 
         let object = this;
         setTimeout(function () {
@@ -61,7 +52,7 @@ class ViewComponent extends BaseComponent{
         new Promise(function (resolve, reject) {
 
             let template = '<div id="screen-container"><div class="screen"  id="login-screen"><div class=" spalsh-screen-image  spalsh-screen "><div id="firebaseui-auth-container"></div></div></div></div>';
-            document.getElementById('app').innerHTML = template;
+            selfObject.replaceAppScreen( template );
 
             selfObject.getAppClassManager().getFirebaseCompoenent().loadFirebaseAuthenticationFunctions();
             resolve();
@@ -82,13 +73,11 @@ class ViewComponent extends BaseComponent{
 
         new Promise(function (resolve, reject) {
 
-            var app = new Vue({
-                el: '#screen-container',
-                template: '<div id="screen-container"><div class="screen"  id="home-screen"></div></div>'
-            });
+            let template = '<div id="screen-container"><div class="screen"  id="home-screen"></div></div>';
+            selfObject.replaceAppScreen( template );
 
             document.getElementById('home-screen').innerHTML = document.getElementById('nav-menu').innerHTML;
-            consoleAlert( document.getElementById('home-screen').innerHTML );
+
             resolve();
 
         }).then(function () {
